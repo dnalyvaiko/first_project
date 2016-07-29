9ed4e48398614841d39877bcf47a30c80b4f2e43
dnalyvaiko <nalivayko_d@mail.ru>
2016-07-29 07:57:52 +0300

    Add slider

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,59 @@ $(window).resize(function() {
 	wResize()
 });
 
+var lastId,
+    topMenu = $(".navbar-right"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+ 
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 800);
+  e.preventDefault();
+});
+ 
+$(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 500) {
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
+// scroll body to 0px on click
+$('#back-to-top').click(function () {
+  $('#back-to-top').tooltip('hide');
+  $('body,html').animate({
+    scrollTop: 0
+  }, 800);
+  return false;
+});
+
+$('#back-to-top').tooltip('show');
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
